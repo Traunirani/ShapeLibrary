068922f8a6a900db97b93727d5d6bae76c440156
Traunirani <al.mazn@ya.ru>
2023-09-13 19:56:45 +0500

    Добавьте файлы проекта.

---
+++ Shape.cs
@@ -0,0 +1,49 @@
+﻿namespace ShapeLibrary
+{
+    public abstract class Shape
+    {
+        public abstract double CalculateArea();
+    }
+
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        private double sideA;
+        private double sideB;
+        private double sideC;
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+        }
+
+        public override double CalculateArea()
+        {
+            double s = (sideA + sideB + sideC) / 2;
+            return Math.Sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+        }
+
+        public bool IsRightTriangle()
+        {
+            double[] sides = { sideA, sideB, sideC };
+            Array.Sort(sides);
+            return Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) == Math.Pow(sides[2], 2);
+        }
+    }
+}
---
+++ UTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace ShapeLibrary
+{
+    [TestFixture]
+    public class ShapeTests
+    {
+        [Test]
+        public void CircleAreaCalculation()
+        {
+            double radius = 5.0;
+            Circle circle = new Circle(radius);
+            double expectedArea = Math.PI * radius * radius;
+            double actualArea = circle.CalculateArea();
+            Assert.AreEqual(expectedArea, actualArea, 0.001);
+        }
+
+        [Test]
+        public void TriangleAreaCalculation()
+        {
+            double sideA = 3.0;
+            double sideB = 4.0;
+            double sideC = 5.0;
+            Triangle triangle = new Triangle(sideA, sideB, sideC);
+            double expectedArea = 6.0;
+            double actualArea = triangle.CalculateArea();
+            Assert.AreEqual(expectedArea, actualArea, 0.001);
+        }
+
+        [Test]
+        public void RightTriangleCheck()
+        {
+            double sideA = 3.0;
+            double sideB = 4.0;
+            double sideC = 5.0;
+            Triangle triangle = new Triangle(sideA, sideB, sideC);
+            bool isRightTriangle = triangle.IsRightTriangle();
+            Assert.IsTrue(isRightTriangle);
+        }
+    }
+}
